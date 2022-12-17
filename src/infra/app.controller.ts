9b4211b8e4f37b21644386ab7cb8277fee529feb
Igor Santos <falaigors@gmail.com>
2022-12-17 13:57:04 -0300

    create content and notificantionn entities

---
+++ src/infra/app.controller.ts
@@ -0,0 +1,28 @@
+import { randomUUID } from 'node:crypto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from 'src/application/prisma.service';
+import { CreateNotificationBody } from 'src/application/create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    return this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
